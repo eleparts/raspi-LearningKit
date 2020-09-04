cd3b5df61bcba1a02c198ed60d53bd9bf5c583b3
eleparts <nyk.yeon@eleparts.co.kr>
2020-09-04 06:34:16 +0100

    08 추가

--- 08. Switch input/Switch input.c
+++ 08. Switch input/Switch input.c
@@ -1,30 +1,59 @@
+/* 08. Switch input
+*
+* 텍트 스위치와 틸트(볼) 스위치의 상태를 입력받아 LED를 ON/OFF 해 줍니다.
+*
+* 필요 소자
+* 텍트스위치, 틸트 스위치, LED 2ea, 저항 220옴 2ea, 10k옴 2ea
+* 
+* LED : Wpi : 4 | BCM : #23
+* LED : Wpi : 5 | BCM : #24
+* 텍트 스위치 : Wpi : 0 | BCM : #17
+* 틸트 스위치 : Wpi : 2 | BCM : #27
+*
+* ※ Wpi번호는 wiringPi 라이브러리가 사용하는 핀 번호이며, BCM 번호는 GPIO Cobbler Plus V2 보드에 적혀있는 번호입니다.
+* ※ 스위치는 두종류 다 10k옴 저항을 이용하여 풀업회로를 구성해 줍니다. 
+*/
 #include <stdio.h>
 #include <wiringPi.h>
 
-#define	LED1     
-#define	LED2     
-#define	TACTSW   
-#define	BALLSW   
-
-
-
+#define	LED1    4  // #23
+#define	LED2    5  // #24
+#define	TACTSW  0  // #17
+#define	TILTSW  2  // #27
+ 
 int main (void)
 {
-  int value ;
+  int tactsw, tiltsw;
 
-  wiringPiSetup () ;
+  wiringPiSetup();
 
-  for (;;)
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+
+  pinMode(TACTSW, INPUT);
+  pinMode(TILTSW, INPUT);
+
+  while(1)
   {
-    value = analogRead  (Q2W_ABASE + 0) ;
-    //pwmWrite (LED, value * 4) ;
-    printf("value=%d\n",value);
-    if (value>250)
-    {digitalWrite(LED,HIGH);}
-    else
-    {digitalWrite(LED,LOW);}
-    delay (100) ;
+    tactsw = digitalRead(TACTSW);
+    tiltsw = digitalRead(TILTSW);
+
+    if(tactsw == 0)
+    {
+      digitalWrite(LED1, 1);
+    }else{
+      digitalWrite(LED1, 0);
+    }
+    
+    if(tiltsw != 0)
+    {
+      digitalWrite(LED2, 1);
+    }else{
+      digitalWrite(LED2, 0);
+    }
+
+    delay(100);
   }
 
-  return 0 ;
+  return 0;
 }
